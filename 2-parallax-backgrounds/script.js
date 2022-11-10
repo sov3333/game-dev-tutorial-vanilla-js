1999370489acc741f361b2996093281c41e8da67
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-10 15:45:37 +0800

    fix: lesson_2, removed unused var gameFrame, ensure page load b4 start

--- 2-parallax-backgrounds/script.js
+++ 2-parallax-backgrounds/script.js
@@ -4,7 +4,7 @@ const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 
 let gameSpeed = 10;
-let gameFrame = 0;
+// let gameFrame = 0;
 
 const backgroundLayer1 = new Image(); // same as `document.createElement("img");`
 backgroundLayer1.src = '/2-parallax-backgrounds/images/layer-1.png';
@@ -17,60 +17,64 @@ backgroundLayer4.src = '/2-parallax-backgrounds/images/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = '/2-parallax-backgrounds/images/layer-5.png';
 
-const slider = document.getElementById('slider');
-slider.value = gameSpeed;
-const showGameSpeed = document.getElementById('showGameSpeed');
-showGameSpeed.innerHTML = gameSpeed;
-slider.addEventListener('change', function(e){
-    gameSpeed = e.target.value;
+window.addEventListener('load', function(){ // ensure all images/html elements loaded before start game
+
+    const slider = document.getElementById('slider');
+    slider.value = gameSpeed;
+    const showGameSpeed = document.getElementById('showGameSpeed');
     showGameSpeed.innerHTML = gameSpeed;
-});
-
-// Parallax effect is when foreground layer moves faster than the background layer
-// Full tutorial of Lesson 2 from: https://youtu.be/GFO_txvwK_c?t=2587
-
-class Layer {
-    constructor(image, speedModifier) {
-        this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 700;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gameSpeed * this.speedModifier;
-    }
-    update(){
-        this.speed = gameSpeed * this.speedModifier; 
-        if (this.x <= -this.width){
-            this.x = 0;
-        }
-        this.x = this.x - this.speed;
-        // this.x = gameFrame * this.speed % this.width; // this can replace previous 4 lines
-
-    }
-    draw(){
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-    }
-}
-
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
-
-const gameObjects = [layer1, layer2, layer3, layer4, layer5];
-
-function animate() {
-    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT); 
-
-    gameObjects.forEach(object => {
-        object.update();
-        object.draw();
+    slider.addEventListener('change', function(e){
+        gameSpeed = e.target.value;
+        showGameSpeed.innerHTML = gameSpeed;
     });
-
-    gameFrame--;
-    requestAnimationFrame(animate);
-}
-animate();
+    
+    // Parallax effect is when foreground layer moves faster than the background layer
+    // Full tutorial of Lesson 2 from: https://youtu.be/GFO_txvwK_c?t=2587
+    
+    class Layer {
+        constructor(image, speedModifier) {
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * this.speedModifier;
+        }
+        update(){
+            this.speed = gameSpeed * this.speedModifier; 
+            if (this.x <= -this.width){
+                this.x = 0;
+            }
+            this.x = this.x - this.speed;
+            // this.x = gameFrame * this.speed % this.width; // this can replace previous 4 lines
+    
+        }
+        draw(){
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
+    }
+    
+    const layer1 = new Layer(backgroundLayer1, 0.2);
+    const layer2 = new Layer(backgroundLayer2, 0.4);
+    const layer3 = new Layer(backgroundLayer3, 0.6);
+    const layer4 = new Layer(backgroundLayer4, 0.8);
+    const layer5 = new Layer(backgroundLayer5, 1);
+    
+    const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+    
+    function animate() {
+        ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT); 
+    
+        gameObjects.forEach(object => {
+            object.update();
+            object.draw();
+        });
+    
+        // gameFrame--;
+        requestAnimationFrame(animate);
+    }
+    animate();
+    
+});
