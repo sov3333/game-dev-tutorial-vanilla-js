f1e5bd1af5aaa528e0f2aa0f9547d58e158bb171
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-10 15:54:32 +0800

    bookmark lesson_3 video tutorial link

--- 3-enemy-movement-patterns/script.js
+++ 3-enemy-movement-patterns/script.js
@@ -3,3 +3,5 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+
+// continue from: https://youtu.be/GFO_txvwK_c?t=5362
