bc15294e0c20020dcb9064eeca47c1e376dcd80a
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-10 15:06:18 +0800

    save progress lesson_2, refactor to apply all layers w DRY

--- 2-parallax-backgrounds/script.js
+++ 2-parallax-backgrounds/script.js
@@ -43,13 +43,13 @@ class Layer {
     update(){
         this.speed = gameSpeed * this.speedModifier; // if game has constant never-changing scrolling speed, don't need this line
         if (this.x <= -this.width){
-            this.x = this.width + this.x - this.speed;
+            this.x = this.width + this.x2 - this.speed;
         }
         if (this.x2 <= -this.width){
             this.x2 = this.width + this.x - this.speed;
         }
         this.x = Math.floor(this.x - this.speed);
-        this.x2 = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
     }
     draw(){
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
@@ -57,13 +57,21 @@ class Layer {
     }
 }
 
-const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate() {
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT); 
 
-    layer4.update();
-    layer4.draw();
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    });
 
     requestAnimationFrame(animate);
 }
