7aa1715842385f10a013604ef56c7cbcd6fcbccc
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-16 20:24:33 +0800

    feat: endlessly scrolling background

--- 7-side-scroller-game/scripts.js
+++ 7-side-scroller-game/scripts.js
@@ -84,7 +84,24 @@ window.addEventListener('load', function() {
     }
 
     class Background {
-
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById('backgroundImage');
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 720;
+            this.speed = 7;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x+this.width-this.speed, this.y, this.width, this.height); // draw same image twice for illusion of endless scrolling seamless image
+        }
+        update(){
+            this.x -= this.speed; // scroll to the left -20 pixels per frame
+            if (this.x < 0 - this.width) this.x = 0; // if bg scroll off-screen, set horizontal x position back to 0
+        }
     }
 
     function handleEnemies(){
@@ -97,9 +114,12 @@ window.addEventListener('load', function() {
 
     const input = new InputHandler();
     const player = new Player(canvas.width, canvas.height);
+    const background = new Background(canvas.width, canvas.height);
     
     function animate(){
         ctx.clearRect (0,0,canvas.width,canvas.height);
+        background.draw(ctx); // draw background first, so player is visible in front
+        background.update();
         player.draw(ctx);
         player.update(input);
         requestAnimationFrame(animate); // pass in name of parent function to create endless animation loop
