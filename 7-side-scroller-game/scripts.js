c36cdecf693adf16f09beb36c9bf742ed17c00cf
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-16 19:41:31 +0800

    feat: handle keyboard inputs via InputHandler()

--- 7-side-scroller-game/scripts.js
+++ 7-side-scroller-game/scripts.js
@@ -6,7 +6,28 @@ window.addEventListener('load', function() {
     canvas.height = 720;
 
     class InputHandler {
-
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown', (e) => {
+                if ((   e.key === 'ArrowDown' || 
+                        e.key === 'ArrowUp' || 
+                        e.key === 'ArrowLeft' || 
+                        e.key === 'ArrowRight' )
+                        && this.keys.indexOf(e.key) === -1){
+                    this.keys.push(e.key);
+                }
+                // console.log(e.key, this.keys);
+            });
+            window.addEventListener('keyup', (e) => {
+                if (    e.key === 'ArrowDown' || 
+                        e.key === 'ArrowUp' || 
+                        e.key === 'ArrowLeft' || 
+                        e.key === 'ArrowRight' ){
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+                // console.log(e.key, this.keys);
+            });
+        }
     }
 
     class Player {
@@ -25,7 +46,9 @@ window.addEventListener('load', function() {
 
     }
 
+    const input = new InputHandler();
+
     function animate(){
-        
+
     }
 });
