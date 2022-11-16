95aef34b85f25741f127889a61436f38e2cde5db
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-17 07:44:40 +0800

    feat: setup player class

--- 8-state-management/script.js
+++ 8-state-management/script.js
@@ -1,7 +1,13 @@
+import Player from './helpers/player.js';
+
 window.addEventListener('load', function(){
     const loading = document.getElementById('loading');
+    loading.style.display = 'none';
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const player = new Player(canvas.width, canvas.height);
+    player.draw(ctx);
 })
