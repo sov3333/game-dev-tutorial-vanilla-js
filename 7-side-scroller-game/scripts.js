35b2aa3c77534ce5ae2289494d01e9d635ff6f42
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-16 18:48:25 +0800

    init: setup game logic backbone

--- 7-side-scroller-game/scripts.js
+++ 7-side-scroller-game/scripts.js
@@ -4,4 +4,28 @@ window.addEventListener('load', function() {
     const ctx = canvas.getContext('2d');
     canvas.width = 800;
     canvas.height = 720;
+
+    class InputHandler {
+
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+
+    }
+
+    function handleEnemies(){
+        
+    }
+
+    function displayStatusText(){
+
+    }
+
+    function animate(){
+        
+    }
 });
