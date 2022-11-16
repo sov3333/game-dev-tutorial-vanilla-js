71f69e22d8c55dd34409931684b8cd1676d8a19d
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-16 18:46:19 +0800

    init: setup canvas, add images, add .gitignore

---
+++ 7-side-scroller-game/scripts.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    // game goes here
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+});
