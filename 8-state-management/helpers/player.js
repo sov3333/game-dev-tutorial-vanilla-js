9ef1c67d922ef73603085d1938aa1477e33cbbb3
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-17 09:32:02 +0800

    feat: extend states - add jumping state

--- 8-state-management/helpers/player.js
+++ 8-state-management/helpers/player.js
@@ -1,16 +1,18 @@
-import {StandingLeft, StandingRight, SittingLeft, SittingRight, RunningLeft, RunningRight} from './state.js'
+import {StandingLeft, StandingRight, SittingLeft, SittingRight, RunningLeft, RunningRight, JumpingLeft, JumpingRight} from './state.js'
 
 export default class Player {
     constructor(gameWidth, gameHeight){
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-        this.states = [new StandingLeft(this), new StandingRight(this), new SittingLeft(this), new SittingRight(this), new RunningLeft(this), new RunningRight(this)];
+        this.states = [new StandingLeft(this), new StandingRight(this), new SittingLeft(this), new SittingRight(this), new RunningLeft(this), new RunningRight(this), new JumpingLeft(this), new JumpingRight(this)];
         this.currentState = this.states[1];
         this.image = document.getElementById('dogImage');
         this.width = 200;
         this.height = 181.83;
         this.x = this.gameWidth/2 - this.width/2;
         this.y = this.gameHeight - this.height;
+        this.vy = 0;
+        this.weight = 0.5;
         this.frameX = 0;
         this.frameY = 0;
         this.speed = 0;
@@ -21,12 +23,25 @@ export default class Player {
     }
     update(input){
         this.currentState.handleInput(input);
+        // horizontal movement
         this.x += this.speed;
         if (this.x <= 0) this.x = 0;
         else if (this.x >= this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+        // vertical movement
+        this.y += this.vy;
+        if (!this.onGround()){ // if player is not standing on ground
+            this.vy += this.weight;
+        } else {
+            this.vy = 0;
+        }
+        if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height; // optional check to not let char fall through floor
+
     }
     setState(state){
         this.currentState = this.states[state];
         this.currentState.enter();
+    }
+    onGround(){
+        return this.y >= this.gameHeight - this.height
     }
 }
