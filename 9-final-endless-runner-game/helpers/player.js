662485f4cd8747c7b2fef973f8b021f799f6bed8
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-17 11:59:02 +0800

    feat: add sprite animations

--- 9-final-endless-runner-game/helpers/player.js
+++ 9-final-endless-runner-game/helpers/player.js
@@ -13,13 +13,17 @@ export class Player {
         this.image = player; // this.image = document.getElementById('player'); 
         this.frameX = 0;
         this.frameY = 0;
+        this.maxFrame = 5;
+        this.fps = 30;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
         this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
         this.currentState = this.states[0];
         this.currentState.enter();
     }
-    update(input){
+    update(input, deltaTime){
         this.currentState.handleInput(input);
         // horizontal movement
         this.x += this.speed;
@@ -29,10 +33,17 @@ export class Player {
         if (this.x < 0) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         // vertical movement
-        // if (input.includes('ArrowUp') && this.onGround()) this.vy -= 28;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
+        // sprite animation
+        if (this.frameTimer > this.frameInterval){
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+        } else {
+            this.frameTimer += deltaTime;
+        }
     }
     draw(context){
         // visual aid
--- 9-final-endless-runner-game/helpers/playerStates.js
+++ 9-final-endless-runner-game/helpers/playerStates.js
@@ -17,6 +17,7 @@ export class Sitting extends State {
         this.player = player;
     }
     enter(){
+        this.player.maxFrame = 4;
         this.player.frameY = 5;
     }
     handleInput(input){
@@ -31,6 +32,7 @@ export class Running extends State {
         this.player = player;
     }
     enter(){
+        this.player.maxFrame = 8;
         this.player.frameY = 3;
     }
     handleInput(input){
@@ -48,6 +50,7 @@ export class Jumping extends State {
     }
     enter(){
         if (this.player.onGround()) this.player.vy -= 27;
+        this.player.maxFrame = 6;
         this.player.frameY = 1;
     }
     handleInput(input){
@@ -62,6 +65,7 @@ export class Falling extends State {
         this.player = player;
     }
     enter(){
+        this.player.maxFrame = 6;
         this.player.frameY = 2;
     }
     handleInput(input){
