b0c47865158f8ea8870b4955d5b99db6e2c8d126
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-17 11:48:13 +0800

    feat: add player states

--- 9-final-endless-runner-game/helpers/player.js
+++ 9-final-endless-runner-game/helpers/player.js
@@ -1,3 +1,5 @@
+import { Sitting, Running, Jumping, Falling } from './playerStates.js'
+
 export class Player {
     constructor(game){
         this.game = game;
@@ -9,10 +11,16 @@ export class Player {
         this.weight = 1;
         // javascript automatically creates references to all elements with IDs into the global namespace, using it's ID as a variable name:
         this.image = player; // this.image = document.getElementById('player'); 
+        this.frameX = 0;
+        this.frameY = 0;
         this.speed = 0;
         this.maxSpeed = 10;
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
     }
     update(input){
+        this.currentState.handleInput(input);
         // horizontal movement
         this.x += this.speed;
         if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
@@ -21,7 +29,7 @@ export class Player {
         if (this.x < 0) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         // vertical movement
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 28;
+        // if (input.includes('ArrowUp') && this.onGround()) this.vy -= 28;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
@@ -31,9 +39,13 @@ export class Player {
         context.strokeStyle = 'red';
         context.strokeRect(this.x, this.y, this.width, this.height);
         // draw
-        context.drawImage(this.image, this.width*0,this.height*0,this.width,this.height, this.x,this.y,this.width,this.height);
+        context.drawImage(this.image, this.width*this.frameX,this.height*this.frameY,this.width,this.height, this.x,this.y,this.width,this.height);
     }
     onGround(){
         return this.y >= this.game.height - this.height;
+    }
+    setState(state){
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
