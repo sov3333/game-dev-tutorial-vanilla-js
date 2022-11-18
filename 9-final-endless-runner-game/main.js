8c7cc63c08d21cd184a455d728e1decddbac2bf7
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-18 17:22:49 +0800

    fix: ground enemy animation bug by replace slice()

--- 9-final-endless-runner-game/main.js
+++ 9-final-endless-runner-game/main.js
@@ -52,7 +52,6 @@ window.addEventListener('load', function(){
             }
             this.enemies.forEach(enemy => {
                 enemy.update(deltaTime);
-                if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
             });
             // handle messages
             this.floatingMessages.forEach(message => {
@@ -62,7 +61,6 @@ window.addEventListener('load', function(){
             // handle particles
             this.particles.forEach((particle, index) => {
                 particle.update();
-                if (particle.markedForDeletion) this.particles.splice(index, 1);
             });
             if (this.particles.length > this.maxParticles) {
                 this.particles.length = this.maxParticles;
@@ -70,8 +68,10 @@ window.addEventListener('load', function(){
             // handle collision sprites
             this.collisions.forEach((collision, index) => {
                 collision.update(deltaTime);
-                if (collision.markedForDeletion) this.collisions.splice(index, 1);
             })
+            this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
+            this.particles = this.particles.filter(particle => !particle.markedForDeletion);
+            this.collisions = this.collisions.filter(collision => !collision.markedForDeletion);
             this.floatingMessages = this.floatingMessages.filter(message => !message.markedForDeletion);
         }
         draw(context){
